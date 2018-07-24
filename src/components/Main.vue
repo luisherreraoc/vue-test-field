566186fe3523ab389ff45f39a7c8ce945345fcb4
Luis Acedo <lacedo@oceano.com>
2018-07-24 09:02:09 +0200

    añadido filtro para buscar segun lo indicado

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="wrapper">
-    <div>
-      <label for="search">Indica tu búsqueda: </label>
-      <input id="search" type="text" v-model="busqueda">
+    <div class="search-wrap">
+
+      <div>
+        <label for="search">Indica tu búsqueda: </label>
+        <!-- en los inputs el v-model es el q se encarga del two-way binding -->
+        <input id="search" type="text" v-model="busqueda">            
+      </div>
+
+      <div>
+        <label for="filtro">Buscar según: </label>
+        <select v-model="filtro" name="filtro" id="filtro">
+          <option value="pais">País</option>
+          <option value="apellido">Apellido</option>
+          <option value="nif">NIF</option>
+        </select>
+      </div>
+
     </div>
 
       <div>
+        <p>Mostrar usuarios con estado: </p>
         <label><input type="radio" v-model="isActive" value="todos" />Todos</label>
         <label><input type="radio" v-model="isActive" value="active" />Activo</label>
         <label><input type="radio" v-model="isActive" value="inactive" />No activo</label>      
       </div>      
 
     <ul class="users-wrapper">
+      <!-- al usar v-for, Vue nos pedirá por defecto q a cada valor de la iteration le demos una :key única -->
       <ListadoUsers v-for="user in usersPerPage" 
                     :key="user.id"
                     :user="user"/>
@@ -21,6 +37,8 @@
 
     <div>
       <p>Mostrar por página: </p>
+      <!-- cuando queremos bindear un valor a un input del tipo boolean o number utilizamos el v-bind (:) -->
+      <!-- sin el bind el valor q le pasamos será directamente una string -->
       <label><input type="radio" v-model="mostrar" :value=6 />6</label>
       <label><input type="radio" v-model="mostrar" :value=12 />12</label>
       <label><input type="radio" v-model="mostrar" :value=16 />16</label>      
@@ -48,7 +66,8 @@ export default {
       isActive: "todos",
       busqueda: '',
       page: 1,
-      mostrar: 6
+      mostrar: 6,
+      filtro: 'pais'
     }
   },
   // los valores referenciados desde data se han de llamar como this.
@@ -69,9 +88,22 @@ export default {
     },
     getFilteredUsers() {
       let filter = new RegExp(this.busqueda.normalize('NFD').replace(/[\u0300-\u036f]/g, ""), 'i')
-      return this.getActiveUsers.filter(user => 
-              user.pais.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-              .match(filter))
+
+      let fil = this.filtro;
+
+      if (fil === "pais") {
+        return this.getActiveUsers.filter(user => 
+                user.pais.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+                .match(filter))
+      } else if (fil === "apellido") {
+        return this.getActiveUsers.filter(user => 
+                user.apellido.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+                .match(filter))
+      } else {
+        return this.getActiveUsers.filter(user => 
+                user.NIF.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+                .match(filter))
+      }
     }
   },
   watch: {
@@ -109,6 +141,12 @@ export default {
     margin-left: auto;
     margin-right: auto;
   }
+
+  .search-wrap {
+    display: flex; 
+    justify-content: space-evenly;
+  }
+
   .users-wrapper {
     display: flex;
     justify-content: space-around;
